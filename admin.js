17103cf85af4fbebe03ce8e14c7131eabed1293e
Jonas-Victor950 <Jonasvpsouza950@gmail.com>
2022-09-23 16:14:13 -0300

    quase tudo

---
+++ admin.js
@@ -0,0 +1,44 @@
+// LINK DA API
+const url = 'https://xp41-soundgarden-api.herokuapp.com/events/'
+
+
+function redirecionar() {
+    // função para redirecionar o usuário para a página admin.html ao terminar
+    window.location.href = 'admin.html'
+}
+
+// inserindo LISTA DE EVENTOS NA TELA
+async function listandoEvento() {
+    ;
+    try {
+        const eventos = document.querySelector("#tabelaEventos");
+        const responseEvento = await fetch(url)
+        let ind = 1
+        const listaEventos = await responseEvento.json()
+        listaEventos.forEach((evento) => {
+            
+            let formatoData = new Date(evento.scheduled).toLocaleString()
+            const card =  `
+            <th scope="row">${ind}</th>
+            <td>${formatoData}</td>
+            <td>${evento.name}</td>
+            <td>${evento.attractions}</td>
+            <td>
+                <a href="reservas.html" class="btn btn-dark">ver reservas</a>
+                <a href="editar-evento.html?id=${evento._id}" id="${evento._id}" onclick="mostrarInformacoes()" class="btn btn-secondary">
+                    editar
+                </a>
+                <a href="excluir-evento.html?id=${evento._id}" id="${evento._id}" onclick="mostrarInformacoes()" class="btn btn-danger">
+                    excluir
+                </a>
+            </td>
+        `
+        eventos.innerHTML += card
+        ind++
+        });
+    } catch {
+        console.log('erro ao listar html');
+    }
+}
+
+listandoEvento()
---
+++ excluir-evento.js
@@ -0,0 +1,82 @@
+const urll = 'https://xp41-soundgarden-api.herokuapp.com/events/'
+
+
+const btnDelete = document.getElementsByClassName('btn btn-danger')
+const deleteBtn = btnDelete[0]
+deleteBtn.setAttribute("id","deleteBottom")
+console.log(deleteBtn);
+
+
+const searchBar = window.location.search
+const parametroURL = new URLSearchParams(searchBar)
+const id = parametroURL.get('id')
+const urlId = urll + id
+console.log(id);
+
+function zerarCampos () {
+    document.getElementById('nome').value = ""
+    document.getElementById('banner').value = ""
+    document.getElementById('atracoes').value = ""
+    document.getElementById('descricao').value = ""
+    document.getElementById('data').value = ""
+    document.getElementById('lotacao').value = ""
+}
+
+function redireciona(){
+    window.location.assign("/admin.html");    
+}
+
+
+const nomeEvento = ""
+
+async function mostrarInformacoes() {
+
+    try {
+        const response = await fetch(urlId);
+        const data = await response.json();
+
+        //transforma a data de ISO para data local
+        let eventDate = new Date(data.scheduled).toLocaleString()
+        // pega a informações e atribuem cada uma ao seu respectivo campo por ordem
+        document.getElementById('nome').value = data.name
+        document.getElementById('banner').value = data.poster
+        document.getElementById('atracoes').value = data.attractions
+        document.getElementById('descricao').value = data.description
+        document.getElementById('data').value = eventDate
+        document.getElementById('lotacao').value = data.number_tickets
+        nomeEvento = data.name
+            
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// função DELETE ok
+async function deletar() {
+
+    try {
+
+    const init = {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'}
+    }
+    const response = confirm(`Deseja Realmente excluir o evento ${id}`)
+        if (response) {
+            await fetch(`https://xp41-soundgarden-api.herokuapp.com/events/${id}`, init);
+            
+        }
+        console.log(response.status)
+        console.log('excluiu')
+        alert(`Você excluiu o evento ${nomeEvento}`)
+        zerarCampos();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+mostrarInformacoes()
+
+deleteBtn.onclick = (event) =>{
+    event.preventDefault()
+    deletar()
+}
