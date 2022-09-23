92be3cfbc48420dee2ed82c34f7b02230e8952c5
Jonas-Victor950 <Jonasvpsouza950@gmail.com>
2022-09-23 20:24:18 -0300

    Adicionando alerta

--- cadastrar-evento1.js
+++ cadastrar-evento1.js
@@ -11,6 +11,9 @@ async function creatEvent(event) {
       scheduled: scheduledDate.toISOString(),
       number_tickets: document.getElementById("lotacao").value,
     };
+
+    const response = confirm(`Deseja Realmente criar o evento ?`)
+    if(response){
     const response = await fetch(
       "https://xp41-soundgarden-api.herokuapp.com/events",
       {
@@ -19,8 +22,10 @@ async function creatEvent(event) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(arrayEvent),
-      }
-    );
+      })
+      const dados = await response.json();
+      alert(`Você criou o evento ${arrayEvent.name}`)
+    };
   } catch (error) {
     console.log(error);
   }
